7a040f47b87bbcb40d3bf3d7aa47f52a08e803a9
kram766 <36835611+kram766@users.noreply.github.com>
2020-07-18 21:50:01 +0530

    Updates

---
+++ precache-manifest.d5c39e72d331883792e8bb92823f78a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a333622476876d341b3a57d28d070b",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "5108dacd9e2aa666b094",
+    "url": "/react_project/static/css/main.d52cffdb.chunk.css"
+  },
+  {
+    "revision": "fda080890b3b57c4e827",
+    "url": "/react_project/static/js/2.17c860ac.chunk.js"
+  },
+  {
+    "revision": "a23cfe14588c664c98e2d6152ad3e7b1",
+    "url": "/react_project/static/js/2.17c860ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5108dacd9e2aa666b094",
+    "url": "/react_project/static/js/main.c75f096f.chunk.js"
+  },
+  {
+    "revision": "c345bb8d3c5d62711cf3",
+    "url": "/react_project/static/js/runtime-main.bc857831.js"
+  }
+]);
